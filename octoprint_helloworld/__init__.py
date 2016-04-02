20fbaaf7ecc3639c33a0dc4f63e6c77ac1f92f23
Christopher Hunt <huntcg@outlook.com>
2016-04-02 19:10:12 +0000

    New Commit 1 To update from improper push path.

---
+++ octoprint_helloworld/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+class HelloWorldPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.TemplatePlugin):
+	def on_after_startup(self):
+		self._logger.info("Hello World!")
+
+__plugin_name__ = "Hello World"
+__plugin_version__ = "1.0.0"
+__plugin_implementation__ = HelloWorldPlugin()
